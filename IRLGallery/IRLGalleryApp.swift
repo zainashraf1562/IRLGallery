c7d493b799028637e1590e8398ce596586b28e03
Noman Ashraf <nomanashraf874@gmail.com>
2024-06-07 19:00:13 -0400

    Initial Commit

---
+++ IRLGallery/IRLGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  IRLGalleryApp.swift
+//  IRLGallery
+//
+//  Created by Noman Ashraf on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct IRLGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
